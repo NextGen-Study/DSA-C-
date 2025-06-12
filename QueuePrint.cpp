a7e9c4ee205d6eb4845d50a633753155abc36ed3
NextGen-Study <madhusudandev27@gmail.com>
2025-06-12 12:25:01 +0530

    updated

---
+++ QueuePrint.cpp
@@ -0,0 +1,27 @@
+#include<queue>
+#include<iostream>
+using namespace std;
+
+void generateBinary(int N) {
+    queue<string> q;
+    q.push("1");
+
+    for (int i = 0; i < N; i++) {
+        string front = q.front();
+        q.pop();
+
+        cout << front << " ";
+        q.push(front + "0");
+        q.push(front + "1");
+    }
+}
+
+int main()
+{
+cout << "Enter the Number";
+int N;
+cin >> N;
+generateBinary(N);
+
+
+}
